eea414799431a9e761676fe5a093fea9226b42b6
moonkorea00 <moonkorea00@gmail.com>
2022-07-26 20:50:14 +0900

    fix: hashroute to browserrouter

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,19 @@
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Blog from './components/Blog';
 import NotFound from './components/NotFound';
 function Router() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:category/:path" element={<Blog />} />
         <Route path="/page-not-found" element={<NotFound />} />
-        <Route path="*" element={<Navigate to='page-not-found' replace />} />
+        <Route path="*" element={<Navigate to="page-not-found" replace />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
