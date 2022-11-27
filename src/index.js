fcad9f4f22d8577236557040d3bf8cb74ea36b10
moonkorea00 <moonkorea00@gmail.com>
2022-11-27 20:52:38 +0900

    chore : remove unused packages

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
-import rootReducer from './modules';
-import { applyMiddleware, createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import ReduxThunk from 'redux-thunk';
-import logger from 'redux-logger';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 import GlobalStyle from './styles/GlobalStyle';
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk,logger))
-);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
       <Router />
     </ThemeProvider>
-  </Provider>
+  </>
 );
