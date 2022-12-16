18e48571f955699b7f208e7e91e48a17323c258d
moonkorea00 <moonkorea00@gmail.com>
2022-12-16 20:36:48 +0900

    feat : generateSiderJson script

---
+++ public/script/generateSiderJSON.cjs
@@ -0,0 +1,43 @@
+const { join } = require('path');
+const fs = require('fs');
+const matter = require('gray-matter');
+
+const INITIAL_SIDER_LIST = [
+  { name: '브라우저 / CS', variant: 'Browser-CS' },
+  { name: '자바스크립트', variant: 'Javascript' },
+  { name: '리액트', variant: 'React' },
+  { name: 'AWS', variant: 'AWS' },
+];
+const CATEGORY_VARIANT = ['Browser-CS', 'Javascript', 'React', 'AWS'];
+
+const generateSiderJSON = () => {
+  const postsDir = join(process.cwd(), '/src/_posts');
+  const fileNames = fs.readdirSync(postsDir);
+
+  const frontMatter = fileNames.map(fileName => {
+    const filePath = join(postsDir, fileName);
+    const metaData = fs.readFileSync(filePath, 'utf8');
+
+    const path = fileName.replace(/\.mdx$/, '');
+    const { data } = matter(metaData);
+
+    return {
+      path,
+      ...data,
+    };
+  });
+
+  const filterByCategory = cat =>
+    frontMatter.filter(post => post.category === cat);
+
+  const siderData = INITIAL_SIDER_LIST.map((el, i) => {
+    if (el.variant === CATEGORY_VARIANT[i]) {
+      return { ...el, posts: filterByCategory(el.variant) };
+    }
+  });
+
+  const siderJSON = JSON.stringify(siderData);
+  fs.writeFileSync('public/script/sider.json', siderJSON, { flag: 'w' });
+};
+
+generateSiderJSON();
