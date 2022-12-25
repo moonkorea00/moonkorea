681c814ff897675853dd0e0b27a4df42a4b68953
moonkorea00 <moonkorea00@gmail.com>
2022-12-26 04:34:05 +0900

    style : article component animation on inital render

--- src/components/common/ArticleLayout/ArticleLayout.style.ts
+++ src/components/common/ArticleLayout/ArticleLayout.style.ts
@@ -2,4 +2,14 @@ import styled from 'styled-components';
 
 export const ArticleContainer = styled.section`
   min-width: 320px;
+  animation: fade-in 0.25s;
+
+  @keyframes fade-in {
+    0% {
+      opacity: 0.1;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
 `;
