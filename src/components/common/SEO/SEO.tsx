349675e2c227f978607b329bafecf722015f8d7a
moonkorea00 <moonkorea00@gmail.com>
2023-03-11 19:40:30 +0900

    fix : meta tag props index page

--- src/components/common/SEO/SEO.tsx
+++ src/components/common/SEO/SEO.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 interface SEOProps {
-  metaData: { id?: string; title?: string; excerpt?: string };
+  metaData: { id?: string; title?: string; excerpt?: string } | null;
 }
 
 const SEO = ({ metaData }: SEOProps) => {
@@ -9,28 +9,26 @@ const SEO = ({ metaData }: SEOProps) => {
   const DESCRIPTION = 'moonkorea 개발 블로그입니다.';
 
   return (
-    <>
-      <Head>
-        <title>{metaData.title || TITLE}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metaData.excerpt || DESCRIPTION} />
-        <meta
-          property="og:url"
-          content={
-            `${process.env.NEXT_PUBLIC_DOMAIN_URL}/${metaData.id}` ||
-            `${process.env.NEXT_PUBLIC_DOMAIN_URL}`
-          }
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content={TITLE} />
-        <meta property="og:title" content={metaData.title || TITLE} />
-        <meta
-          property="og:description"
-          content={metaData.excerpt || DESCRIPTION}
-        />
-        <meta property="og:image" content={process.env.NEXT_PUBLIC_IMG_URL} />
-      </Head>
-    </>
+    <Head>
+      <title>{metaData?.title || TITLE}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={metaData?.excerpt || DESCRIPTION} />
+      <meta
+        property="og:url"
+        content={
+          `${process.env.NEXT_PUBLIC_DOMAIN_URL}/${metaData?.id}` ||
+          `${process.env.NEXT_PUBLIC_DOMAIN_URL}`
+        }
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={TITLE} />
+      <meta property="og:title" content={metaData?.title || TITLE} />
+      <meta
+        property="og:description"
+        content={metaData?.excerpt || DESCRIPTION}
+      />
+      <meta property="og:image" content={process.env.NEXT_PUBLIC_IMG_URL} />
+    </Head>
   );
 };
 
