7191334547a4cf21b8f7e47f43711fafb3932dfe
moonkorea00 <moonkorea00@gmail.com>
2023-04-16 01:35:26 +0900

    feat : custom iframe markdown component

--- src/components/common/Header/Header.style.ts
+++ src/components/common/Header/Header.style.ts
@@ -23,18 +23,3 @@ export const FilledSection = styled.div`
   height: 50px;
   background-color: ${colors.lightOrange};
 `;
-
-export const Heading = styled.h1`
-  font-size: 70px;
-  font-weight: 900;
-  color: ${colors.white};
-  text-align: center;
-
-  @media screen and (max-width: 1024px) {
-    font-size: 55px;
-  }
-
-  @media screen and (max-width: 768px) {
-    font-size: 40px;
-  }
-`;
