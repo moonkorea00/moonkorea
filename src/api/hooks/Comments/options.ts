d3476608997400f6a6a0b8543283a072ab8d3d13
moonkorea00 <moonkorea00@gmail.com>
2023-08-03 15:23:38 +0900

    feat : prefetch comments

--- src/api/hooks/Comments/options.ts
+++ src/api/hooks/Comments/options.ts
@@ -4,6 +4,19 @@ import { CACHE_KEYS } from '../ReactQuery.utils';
 export const getCommentsOptions = {
   queryKey: (id: string) => CACHE_KEYS.comments.detail(id),
   queryFn: service.readComments,
+  options: {
+    cacheTime: 1000 * 60 * 10,
+    staleTime: 1000 * 60 * 5,
+  }
+};
+
+export const prefetchCommentOptions = (id: string) => {
+  return {
+    queryKey: CACHE_KEYS.comments.detail(id),
+    queryFn: service.readComments,
+    cacheTime: 1000 * 60 * 10,
+    staleTime: 1000 * 60 * 5,
+  };
 };
 
 export const createCommentOptions = {
