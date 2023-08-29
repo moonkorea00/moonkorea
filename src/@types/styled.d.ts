51485777bdf5ab5197461c8e3b086c5925e3f692
moonkorea00 <moonkorea00@gmail.com>
2023-08-29 17:43:05 +0900

    feat : add colors type to theme interface

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,7 +1,10 @@
 import 'styled-components';
 
+type Colors = Record<string, string | Colors>;
+
 declare module 'styled-components' {
   export interface DefaultTheme {
+    colors: Colors;
     flexCenter: ThemedCssFunction;
     flexDefault: ThemedCssFunction;
     flexColumn: ThemedCssFunction;
