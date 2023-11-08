4cfabe9382820973c0b730b7c94a4dd6d68b1de7
moonkorea00 <moonkorea00@gmail.com>
2023-11-08 13:20:25 +0900

    style : heading element

--- src/components/Markdown/CustomMarkdown/CustomMarkdown.style.ts
+++ src/components/Markdown/CustomMarkdown/CustomMarkdown.style.ts
@@ -8,9 +8,15 @@ export const BlockQuote = styled.blockquote`
 `;
 
 export const H1 = styled.h1`
+  display: block;
   margin-top: 20px;
   padding-bottom: 8px;
-  font-size: 1.8em;
+  margin-block-start: 0.67em;
+  margin-block-end: 0.67em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-size: 2em;
+  font-weight: 700;
   border-bottom: 1px solid ${({ theme }) => theme.colors.base.gray300};
 
   :hover:after {
@@ -18,32 +24,41 @@ export const H1 = styled.h1`
   }
 
   @media screen and (max-width: 768px) {
-    font-size: 1.6em;
+    font-size: 1.8em;
   }
 `;
 
 export const H2 = styled.h2`
-  font-size: 1.55em;
+  display: block;
+  margin-block-start: 0.83em;
+  margin-block-end: 0.83em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-size: 1.6em;
+  font-weight: 700;
+  padding-bottom: 8px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.base.gray300};
 
   :hover:after {
     content: '#';
   }
 
   @media screen and (max-width: 768px) {
-    font-size: 1.3em;
+    font-size: 1.4em;
   }
 `;
 
 export const H3 = styled.h3`
-  font-size: 1.35em;
-  margin-bottom: -10px;
+  display: block;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-size: 1.17em;
+  font-weight: 700;
 
   :hover:after {
     content: '#';
-  }
-
-  @media screen and (max-width: 768px) {
-    font-size: 1.15em;
   }
 `;
 
