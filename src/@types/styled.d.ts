393ceefb7c1e73e96a28bdeaefa8b3c8203a8064
moonkorea00 <moonkorea00@gmail.com>
2023-12-06 10:54:59 +0900

    refactor : default theme type

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,10 +1,48 @@
 import 'styled-components';
 
-type Colors = Record<string, string | Colors>;
+interface ColorPalette {
+  base: {
+    white: string;
+    black: string;
+    lightOrange: string;
+    pinkGrey: string;
+    darkYellow: string;
+    grayDefault: string;
+    gray100: string;
+    gray200: string;
+    gray300: string;
+    gray400: string;
+    gray500: string;
+    gray600: string;
+    gray700: string;
+    gray800: string;
+  };
+  secondary: {
+    light: string;
+    main: string;
+    dark: string;
+  };
+  action: {
+    default: string;
+    delete: string;
+  };
+  overlay: string;
+  shadow: { default: string };
+}
+
+interface ThemeColors {
+  base: ColorPalette['base'];
+  secondary: ColorPalette['secondary'];
+  action: ColorPalette['action'];
+  overlay: ColorPalette['overlay'];
+  shadow: {
+    default: ColorPalette['shadow']['default'];
+  };
+}
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: Colors;
+    colors: ThemeColors;
     flexCenter: ThemedCssFunction;
     flexDefault: ThemedCssFunction;
     flexColumn: ThemedCssFunction;
