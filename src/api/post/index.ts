cd1d0273521f38c29f3c932a5945d8b87ce174a6
moonkorea00 <moonkorea00@gmail.com>
2024-01-02 22:16:09 +0900

    refactor : modularize code

--- src/api/post/index.ts
+++ src/api/post/index.ts
@@ -4,39 +4,33 @@ import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
 
-import { extractHeadings, nestHeadingWithChildren } from './post.utils';
+import {
+  readFileContent,
+  extractHeadings,
+  nestHeadingWithChildren,
+} from './post.utils';
 import { convertToSlug } from '@utils/markdown';
 
 const postsDir = join(process.cwd(), '/src/_posts');
-const fileNames = fs.readdirSync(postsDir);
+const postFileNames = fs.readdirSync(postsDir);
 
 export const getPostPaths = () => {
-  return fileNames.map(fileName => {
+  const paths = postFileNames.map(fileName => {
     const path = fileName.replace(/\.md$/, '');
-    return {
-      params: {
-        postId: path,
-      },
-    };
+    return { params: { postId: path } };
   });
+
+  return paths;
 };
 
 export const getAllPosts = () => {
-  const postFrontMatter = fileNames.map(fileName => {
-    const filePath = join(postsDir, fileName);
-    const metaData = fs.readFileSync(filePath, 'utf8');
-
+  const postFrontMatter = postFileNames.map(fileName => {
+    const fileContent = readFileContent(postsDir, fileName);
     const id = fileName.replace(/\.md$/, '');
-    const { data } = matter(metaData);
+    const { data } = matter(fileContent);
     const { title, date, tags, description } = data;
 
-    return {
-      id,
-      title,
-      date,
-      tags,
-      description,
-    };
+    return { id, title, date, tags, description };
   });
 
   return postFrontMatter;
@@ -44,37 +38,44 @@ export const getAllPosts = () => {
 
 export const getAllPostsSortedByDate = () => {
   const posts = getAllPosts();
-
   return posts.sort((a: FrontMatter, b: FrontMatter) =>
     a.date > b.date ? -1 : 1
   );
 };
 
 export const getPostById = async (id: string) => {
-  const filePath = join(postsDir, `${id}.md`);
-  const metaData = fs.readFileSync(filePath, 'utf8');
-
-  const { data, content } = matter(metaData);
+  const fileContent = readFileContent(postsDir, `${id}.md`);
+  const { data, content } = matter(fileContent);
 
   const headings = extractHeadings(content);
   const tocTree = nestHeadingWithChildren(headings);
   const headingSlugs = headings.map(heading => convertToSlug(heading.value));
 
-  const toc = {
-    tocTree,
-    headingSlugs,
-  };
+  const toc = { tocTree, headingSlugs };
 
-  return {
-    id,
-    ...data,
-    content,
-    toc,
-  };
+  return { id, ...data, content, toc };
 };
 
-export const getPostTags = () => {
-  const prioritizedTags = [
+const buildTagsCount = () => {
+  const tagsCount = postFileNames.reduce<Record<string, number>>(
+    (tags, fileName) => {
+      const fileContent = readFileContent(postsDir, fileName);
+      const { data } = matter(fileContent);
+
+      data.tags
+        .split(', ')
+        .forEach((tag: string) => (tags[tag] = (tags[tag] || 0) + 1));
+
+      return tags;
+    },
+    {}
+  );
+
+  return tagsCount;
+};
+
+const reorderTags = (tags: Record<string, number>) => {
+  const primaryTags = [
     'JavaScript',
     'TypeScript',
     '블로그',
@@ -84,25 +85,16 @@ export const getPostTags = () => {
     '개발 경험',
   ];
 
-  const tags = fileNames.reduce<Record<string, number>>((tags, fileName) => {
-    const filePath = join(postsDir, fileName);
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const { data } = matter(fileContent);
-
-    data.tags
-      .split(', ')
-      .forEach((tag: string) => (tags[tag] = (tags[tag] || 0) + 1));
-
-    return tags;
-  }, {});
-
   const sortedTags: Record<string, number> = {};
-
-  prioritizedTags.forEach(tag => (sortedTags[tag] = tags[tag]));
-
+  primaryTags.forEach(tag => (sortedTags[tag] = tags[tag]));
   Object.keys(tags).forEach(tag => {
     if (!sortedTags[tag]) sortedTags[tag] = tags[tag];
   });
 
   return sortedTags;
+};
+
+export const getPostTags = () => {
+  const tagsCount = buildTagsCount();
+  return reorderTags(tagsCount);
 };
