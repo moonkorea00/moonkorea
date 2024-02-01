7f0a6997247f87927c438c4ccb7ee370878edb7c
moonkorea00 <moonkorea00@gmail.com>
2024-02-01 23:45:12 +0900

    fix : document reference error

--- src/components/common/Portal/index.tsx
+++ src/components/common/Portal/index.tsx
@@ -1,18 +1,23 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 
 const Portal = ({ children }: PropsWithStrictChildren) => {
-  const portalRef = useRef(document.createElement('div'));
+  const [isMounted, setIsMounted] = useState(false);
+  const portalRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
+    setIsMounted(true);
+    portalRef.current = document.createElement('div');
     document.body.appendChild(portalRef.current);
 
     return () => {
-      document.body.removeChild(portalRef.current);
+      if (portalRef.current) document.body.removeChild(portalRef.current);
     };
   }, []);
 
-  return createPortal(children, portalRef.current);
+  return isMounted && portalRef.current
+    ? createPortal(children, portalRef.current)
+    : null;
 };
 
 export default Portal;
