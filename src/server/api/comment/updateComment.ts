0cdbce3fc39a211d05bfabd69b026ce0d7df8854
moonkorea00 <moonkorea00@gmail.com>
2024-02-01 23:51:41 +0900

    fix : access next auth session with getServerSession

--- src/server/api/comment/updateComment.ts
+++ src/server/api/comment/updateComment.ts
@@ -1,10 +1,12 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { getServerSession } from 'next-auth';
+
 import prisma from '@server/db/client';
-import { getSession } from 'next-auth/react';
-import { Session } from 'next-auth';
+import { authOptions } from '@pages/api/auth/[...nextauth]';
 
 const updateComment = async (req: NextApiRequest, res: NextApiResponse) => {
-  const session: Session | null = await getSession({ req });
+  const session = await getServerSession(req, res, authOptions);
   const { id, body } = req.body;
 
   if (!session) return res.status(401).json({ message: 'Unauthorized' });
