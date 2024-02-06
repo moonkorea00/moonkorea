1874e092b036120763a12c759abb46b56e5dffde
moonkorea00 <moonkorea00@gmail.com>
2024-02-06 18:59:30 +0900

    fix : toggle sider button

--- src/components/Sider/Sider.tsx
+++ src/components/Sider/Sider.tsx
@@ -1,6 +1,5 @@
 import * as S from './Sider.style';
 import SiderCategory from './SiderCategory/SiderCategory';
-import OutsideClickWrapper from '@components/common/OutsideClickWrapper/OutsideClickWrapper';
 
 import SIDER_DATA from '@data/sider.json';
 
@@ -11,19 +10,17 @@ interface SiderProps {
 
 const Sider = ({ isSiderVisible, onCloseSider }: SiderProps) => {
   return (
-    <OutsideClickWrapper onClickHandler={onCloseSider} triggerKey="Escape">
-      <S.Container isSiderVisible={isSiderVisible}>
-        <S.CloseButton onClick={onCloseSider}>&#10005;</S.CloseButton>
-        {SIDER_DATA.map(({ name, posts }) => (
-          <SiderCategory
-            key={name}
-            name={name}
-            posts={posts}
-            onCloseSider={onCloseSider}
-          />
-        ))}
-      </S.Container>
-    </OutsideClickWrapper>
+    <S.Container isSiderVisible={isSiderVisible}>
+      <S.CloseButton onClick={onCloseSider}>&#10005;</S.CloseButton>
+      {SIDER_DATA.map(({ name, posts }) => (
+        <SiderCategory
+          key={name}
+          name={name}
+          posts={posts}
+          onCloseSider={onCloseSider}
+        />
+      ))}
+    </S.Container>
   );
 };
 
